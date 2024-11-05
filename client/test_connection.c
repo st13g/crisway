f823321fbf587754a0f760539cbcf244b1458ae2
st13g <cristdelc@gmail.com>
2024-11-04 23:53:47 -0500

    creating a surface

--- client/test_connection.c
+++ client/test_connection.c
@@ -2,9 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <wayland-client.h>
+#include <wayland-server.h>
+#include <wayland-client-protocol.h>
+#include <wayland-egl.h>
+
+
 
 struct wl_display *display = NULL;
 struct wl_compositor *compositor = NULL;
+struct wl_surface *surface;
+struct wl_shell *shell = NULL;
+struct wl_shell_surface *shell_surface;
+
 
 static void
 global_registry_handler(void *data, struct wl_registry *registry, uint32_t id,
@@ -52,6 +61,35 @@ int main(int argc, char **argv) {
     } else {
 	fprintf(stderr, "Found compositor\n");
     }
+
+
+
+  surface = wl_compositor_create_surface(compositor);
+  if (surface == NULL) {
+    fprintf(stderr, "Ca'nt create a surface\n");
+    exit(1);
+  }
+  else {
+    fprintf(stderr, "Created surface\n");
+  }
+
+  if (shell == NULL) {
+    fprintf(stderr, "Havent got a wayland shell\n");
+    exit(1);
+  }
+
+  shell_surface = wl_shell_get_shell_surface(shell, surface);
+  if (shell_surface == NULL) {
+    fprintf(stderr, "Can't create shell surface\n");
+  }
+  else {
+    fprintf(stderr, "created shell surface\n");
+  }
+  wl_shell_surface_set_toplevel(shell_surface);
+
+
+
+    
 
     wl_display_disconnect(display);
     printf("disconnected from display\n");
