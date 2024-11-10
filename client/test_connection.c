0cd78c269fe4e34bd0cca8673c883178c650c00c
st13g <cristdelc@gmail.com>
2024-11-09 23:49:54 -0500

    shell surface

--- client/test_connection.c
+++ client/test_connection.c
@@ -6,25 +6,25 @@
 #include <wayland-client-protocol.h>
 #include <wayland-egl.h>
 
-
-
 struct wl_display *display = NULL;
 struct wl_compositor *compositor = NULL;
 struct wl_surface *surface;
 struct wl_shell *shell = NULL;
 struct wl_shell_surface *shell_surface;
 
-
 static void
 global_registry_handler(void *data, struct wl_registry *registry, uint32_t id,
 	       const char *interface, uint32_t version)
 {
-    printf("Got a registry event for %s id %d\n", interface, id);
-    if (strcmp(interface, "wl_compositor") == 0)
+    if (strcmp(interface, "wl_compositor") == 0) {
         compositor = wl_registry_bind(registry, 
 				      id, 
 				      &wl_compositor_interface, 
 				      1);
+    } else if (strcmp(interface, "wl_shell") == 0) {
+        shell = wl_registry_bind(registry, id,
+                                 &wl_shell_interface, 1);
+    }
 }
 
 static void
@@ -52,7 +52,6 @@ int main(int argc, char **argv) {
     wl_registry_add_listener(registry, &registry_listener, NULL);
 
     wl_display_dispatch(display);
-    printf("this is not predefined");
     wl_display_roundtrip(display);
 
     if (compositor == NULL) {
@@ -62,34 +61,26 @@ int main(int argc, char **argv) {
 	fprintf(stderr, "Found compositor\n");
     }
 
+    surface = wl_compositor_create_surface(compositor);
+    if (surface == NULL) {
+	fprintf(stderr, "Can't create surface\n");
+	exit(1);
+    } else {
+	fprintf(stderr, "Created surface\n");
+    }
 
-
-  surface = wl_compositor_create_surface(compositor);
-  if (surface == NULL) {
-    fprintf(stderr, "Ca'nt create a surface\n");
-    exit(1);
-  }
-  else {
-    fprintf(stderr, "Created surface\n");
-  }
-
-  if (shell == NULL) {
-    fprintf(stderr, "Havent got a wayland shell\n");
-    exit(1);
-  }
-
-  shell_surface = wl_shell_get_shell_surface(shell, surface);
-  if (shell_surface == NULL) {
-    fprintf(stderr, "Can't create shell surface\n");
-  }
-  else {
-    fprintf(stderr, "created shell surface\n");
-  }
-  wl_shell_surface_set_toplevel(shell_surface);
-
-
-
-    
+    if (shell == NULL) {
+	fprintf(stderr, "Haven't got a Wayland shell\n");
+	exit(1);
+    }
+    shell_surface = wl_shell_get_shell_surface(shell, surface);
+    if (shell_surface == NULL) {
+	fprintf(stderr, "Can't create shell surface\n");
+	exit(1);
+    } else {
+	fprintf(stderr, "Created shell surface\n");
+    }
+    wl_shell_surface_set_toplevel(shell_surface);
 
     wl_display_disconnect(display);
     printf("disconnected from display\n");
